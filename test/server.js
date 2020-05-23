fea8719e7df57c4bfb785a2f9bda7818fcd5d7cc
Austin Fisher <Shizzle@MacBook-Pro.local>
2020-05-22 17:01:10 -0700

    added travis.yml

---
+++ test/server.js
@@ -0,0 +1,11 @@
+const mocha = require('mocha');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const should = chai.should();
+const expect = chai.expect;
+
+describe('server.js', function() {
+    it('should start server', function() {
+        
+    })
+})
