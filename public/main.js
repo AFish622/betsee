38634758e031e1a7e96a8243451ddc63c862f399
Austin Fisher <Shizzle@MacBook-Pro.local>
2020-05-23 09:34:21 -0700

    added inital html for sign up

---
+++ public/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+function clickOnSignUpSubmit() {
+    $('.sign-up-form').submit(function(event) {
+        event.preventDefault();
+        let userName = $('.username-input').val();
+        let password = $('.password-input').val();
+        let firstName = $('.first-name-input').val();
+        let lastName = $('.last-name-input').val();
+        sendNewUserRequest(userName, password, firstName, lastName);
+    })
+}
+
+function sendNewUserRequest(username, password, firstName, lastName) {
+    let newUser = {
+        userName: username,
+        password: password,
+        firstName: firstName,
+        lastName: lastName
+    }
+    console.log(newUser)
+}
+
+clickOnSignUpSubmit();
