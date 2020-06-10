98541d03351953852850e128fe299f1ca0eacc05
Austin Fisher <Shizzle@MacBook-Pro.local>
2020-06-10 10:16:18 -0700

    added inital auth with server working

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
 'use strict';
 // need to link to DB
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/betsee-users';
+
 exports.PORT = process.env.port || 8080;
 
 //secrets
-// exports.JWT_SECRET = process.env.JWT_SECRET;
-// exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '14d';
+exports.JWT_SECRET = process.env.JWT_SECRET;
+exports.JWT_EXPIRY = process.env.JWT_EXPIRY || '14d';
